3606da61d0a13ddda5238befa448ffbfc4cbe5af
Ayed49 <ayedbabu60@gmail.com>
2024-10-27 21:04:24 +0600

    first commit

--- src/component/FeaturedJobs/FeaturedJobs.jsx
+++ src/component/FeaturedJobs/FeaturedJobs.jsx
@@ -5,6 +5,8 @@ import Job from "../Job/Job";
 const FeaturedJobs = () => {
     const [jobs, setJobs] = useState([]);
 
+    const [dataLength, setdataLenght] = useState(4);
+
     useEffect( () =>{
         fetch('jobs.json')
         .then(res => res.json())
@@ -19,10 +21,15 @@ const FeaturedJobs = () => {
             </div>
             <div className="grid grid-cols-2 gap-4">
                 {
-                   jobs.map(job => <Job
+                   jobs.slice(0, dataLength).map(job => <Job
                    key={jobs.id}
                    job={job}></Job>) 
                 }
+            </div>
+            <div className={dataLength === jobs.length && 'hidden'}>
+                <button
+                onClick={() => setdataLenght(jobs.length)}
+                className="btn btn-primary">See All Jobs</button>
             </div>
         </div>
     );
--- src/component/Job/Job.jsx
+++ src/component/Job/Job.jsx
@@ -1,10 +1,14 @@
+import { MdLocationOn } from "react-icons/md";
+import { RiMoneyDollarCircleFill } from "react-icons/ri";
+
+
 
 
 const Job = ({job}) => {
     const {id, logo, job_title,company_name,remote_or_onsite,location, job_type, salary, job_description, job_responsibility, educational_requirements, experiences} = job;
     return (
         <div className="card card-compact bg-base-100 shadow-xl">
-  <figure>
+  <figure className="p-5">
     <img
       src={logo}
       alt="Shoes" />
@@ -16,8 +20,14 @@ const Job = ({job}) => {
         <button className="px-5 py-2 font-extrabold border rounded mr-4 border-[#7E90FE]">{remote_or_onsite}</button>
         <button className="px-5 py-2 font-extrabold border rounded mr-4 border-[#7E90FE]">{job_type}</button>
     </div>
-    <div className="card-actions justify-end">
-      <button className="btn btn-primary">Buy Now</button>
+    <div className="mt-4 flex">
+        <h2 className="flex gap-2"><MdLocationOn className="text-2xl"/>{location}</h2>
+        
+            <h2 className="flex gap-2 ml-4"><RiMoneyDollarCircleFill className="text-2xl" />Salary : {salary}</h2>
+        
+    </div>
+    <div className="card-actions">
+      <button className="btn btn-primary">View Details</button>
     </div>
   </div>
 </div>
