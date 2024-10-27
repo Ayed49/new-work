94be85731c74e688b51cab65ac251cfe8236d99c
Ayed49 <ayedbabu60@gmail.com>
2024-10-28 00:31:37 +0600

    first commit

--- src/component/JobDetails/JobDetails.jsx
+++ src/component/JobDetails/JobDetails.jsx
@@ -8,8 +8,20 @@ const JobDetails = () => {
    const job = jobs.find(job => job.id === idInt);
    console.log(job);
     return (
-        <div>
+        <div className="max-w-6xl mx-auto">
             <h1>Job Details : {id}</h1>
+            <img src="https://i.ibb.co.com/56rQVrR/bg1.png" alt="" />
+            <div className="grid md:grid-cols-4 gap-4">
+
+
+                <div className="border md:col-span-3">
+                    <div className="border">
+
+                    </div>
+
+                </div>
+
+            </div>
         </div>
     );
 };
